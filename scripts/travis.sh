d772afbe112b2080b5805775257b569fc80f24b0
OrenMe <oren.me@gmail.com>
2018-07-30 16:06:27 +0300

    feat: add automated deploy script

---
+++ scripts/travis.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
+set -ev
+
+yarn install
+
+if [ "${TRAVIS_MODE}" = "lint" ]; then
+  yarn run eslint
+elif [ "${TRAVIS_MODE}" = "flow" ]; then
+  yarn run flow
+elif [ "${TRAVIS_MODE}" = "unitTests" ]; then
+	yarn run test
+elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
+  # update the version
+  # make sure everything is fetched https://github.com/travis-ci/travis-ci/issues/3412
+  git fetch --unshallow
+#  node ./scripts/set-package-version.js
+#  yarn run lint
+#  yarn run flow
+#  yarn run test
+  yarn run release
+
+#  yarn run build
+#  if [[ $(node ./scripts/check-already-published.js) = "not published" ]]; then
+    # write the token to config
+    # see https://docs.npmjs.com/private-modules/ci-server-config
+#    echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
+#    if [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
+#      npm publish --tag canary
+#      echo "Published canary."
+#      curl https://purge.jsdelivr.net/npm/hls.js@canary
+#      echo "Cleared jsdelivr cache."
+#    elif [ "${TRAVIS_MODE}" = "release" ]; then
+#      npm publish
+#      echo "Published."
+#    fi
+#  else
+#    echo "Already published."
+#  fi
+else
+	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
+	exit 1
+fi
