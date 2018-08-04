c589cd234a0a583d5415b49e788b471fea74e036
OrenMe <oren.me@gmail.com>
2018-08-04 22:37:19 +0300

    feat: add NPM and release canary flow

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -10,8 +10,6 @@ elif [ "${TRAVIS_MODE}" = "flow" ]; then
   yarn run flow
 elif [ "${TRAVIS_MODE}" = "unitTests" ]; then
 	yarn run test
-elif [ "${TRAVIS_MODE}" = "deploy" ]; then
-	yarn run build
 elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
   # update the version
   # make sure everything is fetched https://github.com/travis-ci/travis-ci/issues/3412
@@ -21,16 +19,27 @@ elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ];
 #  yarn run lint
 #  yarn run flow
 #  yarn run test
-  yarn run release
-  yarn run build
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-  git remote rm origin
 
-  # Add new "origin" with access token in the git URL for authentication
-  git remote add origin https://${GH_TOKEN}@github.com/OrenMe/playkit-js.git
-  git push --follow-tags --no-verify origin master
-  yarn run publish
+  setup_npm
+  setup_git
+
+  if [ "${TRAVIS_MODE}" = "release" ]; then
+    yarn run release
+    yarn run build
+    yarn run publish
+    yarn publish
+    echo "Published."
+  elif [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
+    canaryVersion=$(git describe --long --tags --always | sed -e 's/\(.*-\)\([[:digit:]]*-.*\)/\canary-\2/g')
+    yarn run release --prerelease ${canaryVersion} --skip.commit=true --skip.changelog=true --skip.tag=true
+    yarn run build
+#    reset the changelog file
+    git checkout -- CHANGELOG.md
+    yarn publish --tag canary
+    echo "Published canary."
+    curl https://purge.jsdelivr.net/npm/playkit-js-test@canary
+    echo "Cleared jsdelivr cache."
+  fi
 
 #  yarn run build
 #  if [[ $(node ./scripts/check-already-published.js) = "not published" ]]; then
@@ -53,3 +62,18 @@ else
 	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
 	exit 1
 fi
+
+setup_npm() {
+  # write the token to config
+  # see https://docs.npmjs.com/private-modules/ci-server-config
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
+}
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git remote rm origin
+
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://${GH_TOKEN}@github.com/OrenMe/playkit-js.git
+}
