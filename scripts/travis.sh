ce1352d578a66519478553a152349455e6bc08d3
OrenMe <oren.me@gmail.com>
2018-08-15 13:58:40 +0300

    build: tidy up build scripts

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,21 +2,6 @@
 # https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
 set -ev
 
-#setup_npm() {
-#  # write the token to config
-#  # see https://docs.npmjs.com/private-modules/ci-server-config
-#  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
-#}
-#
-#setup_git() {
-#  git config --global user.email "travis@travis-ci.org"
-#  git config --global user.name "Travis CI"
-#  git remote rm origin
-#
-#  # Add new "origin" with access token in the git URL for authentication
-#  git remote add origin https://${GH_TOKEN}@github.com/OrenMe/playkit-js.git
-#}
-
 yarn install
 
 if [ "${TRAVIS_MODE}" = "lint" ]; then
@@ -26,17 +11,7 @@ elif [ "${TRAVIS_MODE}" = "flow" ]; then
 elif [ "${TRAVIS_MODE}" = "unitTests" ]; then
 	yarn run test
 elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
-  # update the version
-  # make sure everything is fetched https://github.com/travis-ci/travis-ci/issues/3412
-  echo "Update git source"
-  git fetch --unshallow 1>&2
-  if [ "${TRAVIS_MODE}" = "release" ]; then
-#    echo "Run standard-version"
-#    yarn run release
-    echo "Building..."
-    yarn run build
-    echo "Finish building"
-  elif [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
+  if [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
     echo "Run standard-version"
     yarn run release --prerelease canary --skip.commit=true --skip.tag=true
     sha=$(git rev-parse --verify --short HEAD)
@@ -49,10 +24,10 @@ elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ];
     sed -iE "s/$currentVersion/$newVersion/g" CHANGELOG.md
     rm package.jsonE
     rm CHANGELOG.mdE
-    echo "Building..."
-    yarn run build
-    echo "Finish building"
   fi
+  echo "Building..."
+  yarn run build
+  echo "Finish building"
 else
 	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
 	exit 1
